6ada3950d38884b4b252ee9c5caec995b11b58f8
Kevin Auer <kvn35124@uab.edu>
2019-07-12 14:06:03 -0500

    added mouse in and mouse out function

--- app.js
+++ app.js
@@ -5,21 +5,41 @@ let button = document.createElement('button');
 let buttonText = document.createTextNode('Add Sqaure');
 button.appendChild(buttonText);
 document.body.appendChild(button);
+let container = document.createElement('div');
+document.body.appendChild(container);
+container.style.display = 'flex';
+container.style.flexWrap = 'wrap';
+let idCounter = 0;
 
 button.addEventListener('click', function() {
     let box = document.createElement('div');
-    document.body.appendChild(box);
-    box.className = 'box';
+    container.appendChild(box);
+    box.id = idCounter;
+    idCounter ++;
+    box.addEventListener('click', function() {
+        console.log(box.id);
+    })
     box.style.backgroundColor = 'black';
-    box.style.border = '1px';
     box.style.height = '5em';
     box.style.width = '5em';
     box.style.margin = '1em';
-    box.idName = '1';
     box.addEventListener('click', function() {
         let colorArray = ['aqua', 'beige', 'blue', 'chocolate', 'dark gray', 'dark red', 'gold', 'lime'];
         let randomColorGenerator = Math.floor(Math.random() * colorArray.length);
         box.style.backgroundColor = colorArray[randomColorGenerator];
+    })
+    box.addEventListener('mouseover', function() {
+        let boxText = document.createTextNode(idCounter);
+        box.appendChild(boxText);
+        box.addEventListener('mouseout', function() {
+        box.removeChild(boxText);
+        })
+    })
+    box.addEventListener('dblclick', function() {
+        if (id % 2 == 0)
+           box.removeChild();
+        else 
+            box.removeChild()
     })
 })
 
